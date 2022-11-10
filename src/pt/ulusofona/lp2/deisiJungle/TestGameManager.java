b0671d1d349f9b2bdebd02a2b137198f99b9ed02
Diogo <diomferreira@gmail.com>
2022-11-10 23:33:38 +0000

    Submissao Final

--- src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestGameManager {
+public class TestGameManager  {
 
     @Test
     public void TestMoveCurrentPlayer() {
